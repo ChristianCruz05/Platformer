6bc570e57ccb9c2910c3674fc4a323f98b2724dc
Mark Christian Cruz <mcrcruz15@gmail.com>
2023-05-03 15:18:03 +0800

    removed double jump

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -10,6 +10,8 @@ public class PlayerMove : MonoBehaviour
     public float jumpForce = 1f;
     public GameObject particles;
     float horizontalMovement;
+
+    bool isGrounded = false;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -17,15 +19,30 @@ public class PlayerMove : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
             Instantiate(particles, transform.position, Quaternion.identity);
         }
     }
     void FixedUpdate()
     {
         horizontalMovement = Input.GetAxis("Horizontal") * movespeed * Time.deltaTime;
-        transform.Translate(horizontalMovement, 0, 0);
+        transform.Translate(horizontalMovement, 0, 0); //tranform, move on its own
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "ground")
+        {
+            isGrounded = true;
+        }
+        if (collision.transform.tag == "PowerUp")
+        {
+            Destroy(collision.transform.gameObject);
+            jumpForce *= 2 ;
+
+        }
     }
 }
