e05edf06e74e84820d703972de062776af0b5a75
Mark Christian Cruz <mcrcruz15@gmail.com>
2023-05-11 22:54:50 +0800

    added power ups and health potion

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,20 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class PlayerMove : MonoBehaviour
 {
+    //public float maxSpeed;
+    //public float maxJump;
+
     public float movespeed = 5f;
     public Rigidbody2D rb;
     public float jumpForce = 1f;
-    public GameObject particles;
     float horizontalMovement;
 
+    TrailRenderer trail;
+    public GameObject particles;
+    Color originalTrailColor;
+    Color originalPlayerColor;
+    SpriteRenderer playerSprite;
+
     bool isGrounded = false;
+
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public TextMeshProUGUI HealthText;
+
+    
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        trail = GetComponentInChildren<TrailRenderer>();
+        HealthText.SetText("Health: " + currentHealth.ToString());
+        originalTrailColor = trail.startColor;
+        playerSprite = GetComponent<SpriteRenderer>();
+        originalPlayerColor = playerSprite.color;
     }
 
     private void Update()
@@ -25,6 +49,7 @@ public class PlayerMove : MonoBehaviour
             isGrounded = false;
             Instantiate(particles, transform.position, Quaternion.identity);
         }
+        HealthText.SetText("Health: " + currentHealth.ToString());
     }
     void FixedUpdate()
     {
@@ -38,11 +63,69 @@ public class PlayerMove : MonoBehaviour
         {
             isGrounded = true;
         }
-        if (collision.transform.tag == "PowerUp")
+        if (collision.transform.tag == "JumpPowerUp")
         {
             Destroy(collision.transform.gameObject);
-            jumpForce *= 2 ;
+            //jumpForce *= 2 ;
+            StartCoroutine(JumpBoost());
+            Debug.Log("Jump");
+        }
+        if (collision.transform.tag == "SpeedPowerUp")
+        {
+            Destroy(collision.transform.gameObject);
+            //movespeed *= 2;
+            StartCoroutine(SpeedUp());
+            Debug.Log("Speed");
 
         }
+        if (collision.transform.tag == "HealthPotion")
+        {
+            if(currentHealth < maxHealth)
+            {
+                currentHealth += 10;
+            }
+            
+            Destroy(collision.transform.gameObject);
+            Debug.Log("Health");
+        }
+        if (collision.transform.tag == "Bullet")
+        {
+            StartCoroutine(TakeDamage());
+            
+            Destroy(collision.transform.gameObject);
+            Debug.Log("Bullet");
+        }
+    }
+
+    private IEnumerator TakeDamage()
+    {
+        playerSprite.color = Color.red;
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+        if (currentHealth > 0)
+        {
+            currentHealth -= 10;
+        }
+        yield return new WaitForSeconds(0.2f);
+        playerSprite.color = originalPlayerColor;
+
+    }
+    private IEnumerator SpeedUp()
+    {
+        trail.startColor = Color.blue;
+        movespeed *= 2 ;
+        yield return new WaitForSeconds(5f);
+        movespeed /= 2;
+        trail.startColor = originalTrailColor;
+    }
+    private IEnumerator JumpBoost()
+    {
+        trail.startColor = Color.red;
+        jumpForce *= 2;
+        yield return new WaitForSeconds(5f);
+        jumpForce /= 2;
+        trail.startColor = originalTrailColor;
     }
 }
---
+++ Assets/Spawner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject bullet;
+    public float interval = 3f;
+    float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer +=Time.deltaTime;
+        while(timer >= interval)
+        {
+            Instantiate(bullet);
+            timer -= interval;
+        }
+    }
+}
